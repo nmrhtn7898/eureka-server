db9ee19ceafd3feb313119de7212eab2bd54bf1b
syj <nmrhtn7898@naver.com>
2020-07-19 22:43:39 +0900

    add docker settings

---
+++ src/main/docker/run.sh
@@ -0,0 +1,6 @@
+#!/bin/sh
+echo "*******************************************"
+echo "run spring cloud eureka server"
+echo $RUN_MESSAGE
+echo "*******************************************"
+java -jar app.jar
